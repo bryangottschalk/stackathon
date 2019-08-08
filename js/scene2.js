4ba243447814f15b448d337cd34a9d73bf48bca7
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-08-08 15:43:29 -0500

    add createBall function

--- js/scene2.js
+++ js/scene2.js
@@ -4,33 +4,27 @@ class scene2 extends Phaser.Scene {
   }
   preload() {
     this.load.image('player1', 'images/player1.png');
-    this.load.image('face', 'images/face.png');
+    this.load.image('ball', 'images/face.png');
   }
   create() {
+    /* GRID */
+
     // const agrid = new AlignGrid({ scene: this, rows: 11, cols: 11 });
     // agrid.showNumbers();
+
+    /* EVENT LISTENERS */
     this.input.keyboard.on('keydown_W', this.movePlayerOneUp, this);
     this.input.keyboard.on('keydown_S', this.movePlayerOneDown, this);
 
     this.input.keyboard.on('keydown_UP', this.movePlayerTwoUp, this);
     this.input.keyboard.on('keydown_DOWN', this.movePlayerTwoDown, this);
 
+    /* TEXT HEADING */
     this.text1 = this.add.text(game.config.width / 2, 50, 'pong');
     this.text1.setOrigin(0.5, 0.5);
 
-    // this.player1 = this.add.image(100, game.config.height / 2, 'player1');
-    // this.player1.displayWidth = 100;
-    // this.player1.scaleY = this.player1.scaleX;
+    /* SET UP PLAYERS */
 
-    // this.player2 = this.add.image(980, game.config.height / 2, 'player1');
-    // this.player2.displayWidth = 100;
-    // this.player2.scaleY = this.player2.scaleX;
-
-    // this.graphics = this.add.graphics();
-    // this.graphics.fillStyle(0xff00ff, 0.5); adds purple circle
-    // this.graphics.fillCircle(game.config.width / 2, game.config.height / 2, 20); //x, y, radius
-
-    //change images to sprites
     this.player1 = this.physics.add.sprite(
       100,
       game.config.height / 2,
@@ -39,6 +33,7 @@ class scene2 extends Phaser.Scene {
     this.player1.displayWidth = 100;
     this.player1.scaleY = this.player1.scaleX;
     this.player1.setImmovable();
+    this.player1.body.collideWorldBounds = true;
 
     this.player2 = this.physics.add.sprite(
       980,
@@ -48,21 +43,10 @@ class scene2 extends Phaser.Scene {
     this.player2.displayWidth = 100;
     this.player2.scaleY = this.player2.scaleX;
     this.player2.setImmovable();
+    this.player2.body.collideWorldBounds = true;
 
-    this.ball = this.physics.add.sprite(
-      game.config.width / 2,
-      game.config.height / 2,
-      'face'
-    );
-    console.log('player1', this.player1);
-    this.physics.add.collider(this.player1, this.ball);
-    this.physics.add.collider(this.player2, this.ball);
-
-    this.ball.setVelocity(600, 0);
-    this.ball.setBounce(1, 0);
-    this.ball.setGravityX(200); // green line shows where the gravity's going
-    const cursors = this.input.keyboard.createCursorKeys();
-    console.log('TCL: scene2 -> create -> cursors ', cursors);
+    /* BALL */
+    this.createBall(game.config.width / 2, game.config.height / 2);
   }
   movePlayerOneUp() {
     this.player1.y -= 50;
@@ -76,7 +60,25 @@ class scene2 extends Phaser.Scene {
   movePlayerTwoDown() {
     this.player2.y += 50;
   }
+  createBall(x, y) {
+    const ball = this.physics.add.sprite(
+      game.config.width / 2,
+      game.config.height / 2,
+      'ball'
+    );
+    ball.body.collideWorldBounds = true;
+    this.physics.add.collider(this.player1, ball);
+    this.physics.add.collider(this.player2, ball);
+
+    ball.setVelocity(600, 0);
+    ball.setBounce(1, 0);
+    ball.setGravityX(200); // green line shows where the gravity's going
+  }
+
   update() {
+    // if (this.ball.body.blocked.left) {
+    //   console.log('player 2 scores');
+    // }
     // const p1Keys = this.input.keyboard.createCursorKeys();
     // const p2Keys = this.input.keyboard.createCursorKeys();
     // if (p2Keys.down.isDown) {
