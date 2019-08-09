bfec9eef4d2fb89ee09ac50ee75143a928e64fd0
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-08-09 11:28:48 -0500

    chat is working, start on p1direction and p2direction functions

--- client/client.js
+++ client/client.js
@@ -1,11 +1,28 @@
-const writeEvent = (text) => {
-  // <ul element>
-  const parent = document.querySelector('#events');
+// we get access to this file from the io object which is wrapped around the server
+// sends as a string the code for the client side connection
+const socket = io();
 
-  //<li> element
-  const el = document.createElement('li')
+// event listener to receive events from the server
+// pass in the emitted message you want to receive from the server
+
+socket.on('p1direction', direction => {});
+
+socket.on('p2direction', direction => {});
+
+socket.on('message', text => {
+  //listens for message from server
+  const parent = document.getElementById('events');
+  const el = document.createElement('li');
   el.innerHTML = text;
-  parent.appendChild(el)
-}
+  parent.appendChild(el);
+});
 
-writeEvent('Welcome to pong!')
+const chatSubmitted = e => {
+  e.preventDefault(); // don't need to submit and reload the page --> we will handle the data ourselves
+  const input = document.querySelector('#chat');
+  const text = input.value;
+  input.value = '';
+  socket.emit('message', text); //send message to server with payload of string text
+};
+
+document.querySelector('#chat-form').addEventListener('submit', chatSubmitted);
--- server/server.js
+++ server/server.js
@@ -2,26 +2,33 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io').listen(server);
-const path = require('path')
+const path = require('path');
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, '..', 'client')));
 
-io.on('connection', (socket) => {
-  socket.emit('message', 'Hi, you are connected!')
-})
-// app.use('/js', express.static(__dirname.join('/js')));
+io.on('connection', socket => {
+  // handle messages from any client
+  console.log('Someone connected', socket.id);
+  socket.emit('message', 'Welcome from the server!'); // emits to one person
 
-// app.get('/', function(req, res) {
-//   res.sendFile(__dirname + '/index.html');
-// });
+  socket.on('message', text => {
+    //handle messages from single client
+    io.emit('message', text); // send chat message to everyone that is connected, included client itself
+    // if we did socket.emit it would send the message to a particular client
+  });
 
-// app.get('/', function(req, res) {
-//   res.sendFile(__dirname + '/index.html');
-// });
+  socket.on('p1direction', direction => {
+    io.emit('p1direction', direction);
+  });
 
-server.on('error', (err) => {
-  console.error('Server error:', err)
-})
+  socket.on('p2direction', direction => {
+    io.emit('p2direction', direction);
+  });
+});
+
+server.on('error', err => {
+  console.error('Server error:', err);
+});
 
 server.listen(8081, function() {
   // Listens to port 8081
